de9117407d8bc60fcf933b454d1695f6220cab79
Calpinosss <calpun21@gmail.com>
2026-09-09 12:39:51 +0700

    HW2

---
+++ HW2/Q1-counter/counter.js
@@ -0,0 +1,14 @@
+let counter = 0;
+            
+function add() {
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+}
+function reduce() {
+    counter--;
+    document.querySelector('h1').innerHTML = counter;
+    }
+function reset(){
+    counter = 0;
+    document.querySelector('h1').innerHTML = counter;
+}
